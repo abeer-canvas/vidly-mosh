265472eefddb72a41ac6685757478c515da5712b
Abeer <abeer.technology@gmail.com>
2020-11-15 12:33:04 +0600

    added vidly project as a git repository
    all the changes from the beginning will be as it is

---
+++ models/genre.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Joi = require('joi');
+
+const Genre = mongoose.model('Genre', new mongoose.Schema({
+    name: {
+        type: String,
+        required: true,
+        minlength: 5,
+        maxlength: 50
+    }
+}));
+
+// Validation function
+function validateGenres(genre){
+    const schema = Joi.object({
+        name: Joi.string()
+                .min(3)
+                .required()
+    });
+    return schema.validate(genre);
+}
+
+exports.Genre = Genre;
+exports.validate = validateGenres;
